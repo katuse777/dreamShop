e19885d9965494b796683a615dce68f3990c04b3
itumeleng <seralakagiso88@gmail.com>
2025-11-26 20:06:55 +0200

    I have just created a Spring Project for the first time. This project is fresh and not code has been written yet.

---
+++ src/test/java/edu/learningspring/dreamshops/DreamShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.learningspring.dreamshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
